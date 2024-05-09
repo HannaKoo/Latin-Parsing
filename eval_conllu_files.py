bd51b82f60ee207802c3518374dff7186852d5fe
Hanna-Mari Kupari <hmknie@utu.fi>
2024-05-09 16:02:39 +0300

    Evaluation metric works, first test file

--- eval_conllu_files.py
+++ eval_conllu_files.py
@@ -1,16 +1,37 @@
+#!/usr/bin/env python3
+
 # Run eval.py on all .conllu files in Results/conllu_files/
+# Evaluate against test.conllu files in Latin-Variabity/
 # Save the results to Results/Evaluation_metrics/
 
 import pathlib
 import subprocess
 
+ud_tools = "/projappl/project_2008402/tools/"
+MM_golds = pathlib.Path("../../Latin-variability/morpho_harmonization/morpho-harmonized-treebanks")
+
 # Results/conllu_files/*.conllu
 rdir = pathlib.Path("Results/conllu_files")
 wdir = pathlib.Path("Results/Evaluation_metrics")
 
-for conllu_file in rdir.glob('*.conllu'):
-    wfile = wdir/conllu_file.stem/".md"
-    bank = conllu_file.split('_')[3]
-    gold_standard = "../Latin-Variability/" #FIXME
-    with open(wfile, "w") as f:
-        subprocess.run(['eval.py', '-v', gold_standard, conllu_file], stdout=f)
+# case_sensitive requires Python 3.12!
+# for conllu_file in rdir.glob('*.conllu', case_sensitive=False):
+for conllu_file in rdir.glob('*ittb.conllu'):
+    wfile = (wdir/conllu_file.stem).with_suffix(".md")
+    bank = str(conllu_file.stem).split('_')[2]
+    print('bank', bank)
+    print("conllu_file", conllu_file)
+    print('wfile', wfile)
+    # for gold_subdir in MM_golds.glob('*' + bank, case_sensitive=False):
+    for gold_subdir in MM_golds.iterdir():
+        print(gold_subdir)
+        if str(gold_subdir)[-len(bank):].lower() == bank:
+            # Should match only one dir, is there a smart way to do this?
+            print(gold_subdir, bank)
+            gold_standard = gold_subdir/("MM-la_" + bank + "-ud-test.conllu")
+            with open(wfile, "w") as f:
+                subprocess.run([ud_tools + 'eval.py', '-v', 
+                                gold_standard, 
+                                conllu_file], 
+                               stdout=f)
+            break
