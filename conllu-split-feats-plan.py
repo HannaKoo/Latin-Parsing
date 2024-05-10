9782c0009e5e45a04434687f59320c2b7ac307af
Hanna-Mari Kupari <hmknie@utu.fi>
2024-05-10 18:55:28 +0300

    Separate directory reading?

--- conllu-split-feats-plan.py
+++ conllu-split-feats-plan.py
@@ -1,3 +1,10 @@
+import pathlib
+import re
+
+wdir = pathlib.Path("Results/conllu_files")
+rdir = pathlib.Path("Results/conllu_files")
+rdir_je = pathlib.Path("../../jenna/latin-tagger-outputs")
+
 todo = {
         'ITTB': [['Trankit', 'Mega'], ['Stanza', 'Mega', 'pretokenized']], 
         'LLCT': [['Trankit', 'Mega'], ['Stanza', 'Mega', 'pretokenized']],
@@ -6,5 +13,25 @@ todo = {
         'UDante': [['Trankit', 'Mega'], ['Stanza', 'Mega', 'pretokenized']],
         }
 
+# Dream data format: {}
+
 for bank in todo:
     print(todo[bank])
+
+    # load hm files, first only Trankit?
+    for file in rdir.glob('MM_Trankit*'):
+        
+        if todo[bank][0] in file:  # sisältyy?
+
+        # if (todo[bank][0][kaikki] in file) or (todo[bank][1][kaikki] in file):
+            pass
+
+    # load Stanza files separately?:
+    for file in rdir.glob('*_pretokenized-Trankit.conllu'):
+        pass
+    # load jenna files
+    for file in rdir_je.glob('*' + bank.lower() + '*.conllu'):
+        pass
+
+    # load all in variable data?
+
