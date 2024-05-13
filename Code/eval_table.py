89098ab88aefc19db6f6ed129288ee1ea3529e96
Hanna-Mari Kupari <hmknie@utu.fi>
2024-05-13 23:02:49 +0300

    ljust() the file names to make it look like table even in a text editor

--- Code/eval_table.py
+++ Code/eval_table.py
@@ -3,18 +3,15 @@
 # Combine Evaluation_metrics into one big table.
 
 # Only use F1 score (column 3 from 0)
-# Include
 
 import pathlib
 from pprint import pprint
 import csv
 
-include = ['ModelName', 'Tokens', 'UPOS', 'UFeats', 'Lemmas', 'UAS', 'LAS']
+include = ['ModelName'.ljust(47), 'Tokens', 'UPOS', 'UFeats', 'Lemmas', 'UAS', 'LAS']
 
 rdir = pathlib.Path('Results/Evaluation_metrics')
 wdir = pathlib.Path('Results/Evaluation_metrics')
-
-data = []
 
 with open(wdir/'eval_table.tsv', 'w', newline='') as f:
     tsv_writer = csv.writer(f, delimiter='\t')
@@ -24,13 +21,13 @@ with open(wdir/'eval_table.tsv', 'w', newline='') as f:
         with open(file, 'r') as f:
             print( '_'.join( file.stem.split('_')[0:4] ) )
             # f1s.append( '_'.join( file.stem.split('_')[0:4] ) )
-            f1s.append(file.stem)
+            f1s.append(file.stem.ljust(47, '.'))
             single = list(f)
             pprint(single)
             for row in single:
                 row = row.split('|')
                 if row[0].strip() in include:
                     head = row[0]
-                    f1 = row[3]
+                    f1 = row[3].strip()
                     f1s.append(f1)
         tsv_writer.writerow(f1s)
