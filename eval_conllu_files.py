a2936b82211e16ffe4ba3d13dd420c9a5c71564b
Hanna-Mari Kupari <hmknie@utu.fi>
2024-05-13 18:30:34 +0300

    Run eval on new udtaggers

--- eval_conllu_files.py
+++ eval_conllu_files.py
@@ -11,31 +11,31 @@ ud_tools = "/projappl/project_2008402/tools/"
 MM_golds = pathlib.Path("../../Latin-variability/morpho_harmonization/morpho-harmonized-treebanks")
 
 # Results/conllu_files/*.conllu
-rdir = pathlib.Path("Results/conllu_files/raw_pretokenized")
+rdir = pathlib.Path("Results/conllu_files")
 wdir = pathlib.Path("Results/Evaluation_metrics")
 
-banks = ['ittb', 'llct', 'perseus', 'proiel', 'udante']
+# banks = ['ittb', 'llct', 'perseus', 'proiel', 'udante']
 
-for bank in banks:
-    rdir = pathlib.Path("Results/conllu_files")/('vote_' + bank)
-    # case_sensitive requires Python 3.12!
-    # for conllu_file in rdir.glob('*.conllu', case_sensitive=False):
-    for conllu_file in rdir.glob('*.conllu'):
-        wfile = (wdir/conllu_file.stem).with_suffix(".md")
-        bank = str(conllu_file.stem).split('_')[2]
-        print('bank', bank)
-        print("conllu_file", conllu_file)
-        print('wfile', wfile)
-        # for gold_subdir in MM_golds.glob('*' + bank, case_sensitive=False):
-        for gold_subdir in MM_golds.iterdir():
-            print(gold_subdir)
-            if str(gold_subdir).lower().endswith(bank):
-                # Should match only one dir, is there a smart way to do this?
-                print(gold_subdir, bank)
-                gold_standard = gold_subdir/("MM-la_" + bank + "-ud-test.conllu")
-                with open(wfile, "w") as f:
-                    subprocess.run([ud_tools + 'eval.py', '-v', 
-                                    gold_standard, 
-                                    conllu_file], 
-                                stdout=f)
-                break
+# for bank in banks:
+# rdir = pathlib.Path("Results/conllu_files")/('vote_' + bank)
+# case_sensitive requires Python 3.12!
+# for conllu_file in rdir.glob('*.conllu', case_sensitive=False):
+for conllu_file in rdir.glob('MM_udtagger*.conllu'):
+    wfile = (wdir/conllu_file.stem).with_suffix(".md")
+    bank = str(conllu_file.stem).split('_')[2]
+    print('bank', bank)
+    print("conllu_file", conllu_file)
+    print('wfile', wfile)
+    # for gold_subdir in MM_golds.glob('*' + bank, case_sensitive=False):
+    for gold_subdir in MM_golds.iterdir():
+        print(gold_subdir)
+        if str(gold_subdir).lower().endswith(bank):
+            # Should match only one dir, is there a smart way to do this?
+            print(gold_subdir, bank)
+            gold_standard = gold_subdir/("MM-la_" + bank + "-ud-test.conllu")
+            with open(wfile, "w") as f:
+                subprocess.run([ud_tools + 'eval.py', '-v', 
+                                gold_standard, 
+                                conllu_file], 
+                            stdout=f)
+            break
