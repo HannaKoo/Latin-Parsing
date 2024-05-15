4e12883745103ce66ec12838bf93394059a46371
Hanna-Mari Kupari <hmknie@utu.fi>
2024-05-15 15:29:16 +0300

    eval the new voted conllus, make table sorted

--- Code/eval_table.py
+++ Code/eval_table.py
@@ -16,7 +16,7 @@ wdir = pathlib.Path('Results/Evaluation_metrics')
 with open(wdir/'eval_table.tsv', 'w', newline='') as f:
     tsv_writer = csv.writer(f, delimiter='\t')
     tsv_writer.writerow(include)
-    for file in rdir.glob('*.md'):
+    for file in sorted(rdir.glob('*.md')):
         f1s = []
         with open(file, 'r') as f:
             print( '_'.join( file.stem.split('_')[0:4] ) )
