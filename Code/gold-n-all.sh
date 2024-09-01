cbbf96df5130008467c31afcc3a860987d2d9ca0
Hanna-Mari Kupari <hmknie@utu.fi>
2024-09-01 20:44:18 +0300

    Is this the version that works for all five?

--- Code/gold-n-all.sh
+++ Code/gold-n-all.sh
@@ -7,18 +7,20 @@ golddir=../../Latin-variability/morpho_harmonization/morpho-harmonized-treebanks
 
 mkdir -p $wdir
 
-# for treebank in LLCT
-# for treebank in ITTB Perseus PROIEL UDante
-for treebank in PROIEL UDante
+# Puhti: Run module load python-data to get Python 3.10!
+for treebank in ITTB LLCT Perseus PROIEL UDante
 do
-    # Puhti: Run module load python-data to get Python 3.10!
+
+    UD=UD
+    if [ $treebank == LLCT ]
+    then
+        UD=ud  # LLCT udtagger file name has 'ud' instead of 'UD'
+    fi
+
     python Code/gold-extend.py \
     $rdir/vote_${treebank,,}/MM_Trankit*.conllu \
     $rdir/vote_${treebank,,}/MM_Stanza*.conllu \
-    $rdir/vote_${treebank,,}/MM_UD*.conllu \
+    $rdir/vote_${treebank,,}/MM_${UD}*.conllu \
     --gold ${golddir}/UD_Latin-${treebank}/MM-la_${treebank,,}-ud-test.conllu \
     --output $wdir/${treebank}.hypergold.conllu
 done
-    # $rdir/vote_${treebank,,}/MM_ud*.conllu \
-# for others:
-# ud for llct, UD for others
