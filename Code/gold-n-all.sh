253d8222f464c8cfb5e636deaf21129760423b4a
Hanna-Mari Kupari <hmknie@utu.fi>
2024-09-01 20:45:12 +0300

    And this works for circse but breaks others?

--- Code/gold-n-all.sh
+++ Code/gold-n-all.sh
@@ -8,19 +8,28 @@ golddir=../../Latin-variability/morpho_harmonization/morpho-harmonized-treebanks
 mkdir -p $wdir
 
 # Puhti: Run module load python-data to get Python 3.10!
-for treebank in ITTB LLCT Perseus PROIEL UDante
+for treebank in CIRCSE ITTB LLCT Perseus PROIEL UDante
 do
 
+    MM=MM-
     UD=UD
     if [ $treebank == LLCT ]
     then
         UD=ud  # LLCT udtagger file name has 'ud' instead of 'UD'
     fi
 
-    python Code/gold-extend.py \
+    if [ $treebank == CIRCSE ]
+    then
+        golddir=../..
+        MM=""
+    fi
+
+    python3 Code/gold-extend.py \
     $rdir/vote_${treebank,,}/MM_Trankit*.conllu \
     $rdir/vote_${treebank,,}/MM_Stanza*.conllu \
     $rdir/vote_${treebank,,}/MM_${UD}*.conllu \
-    --gold ${golddir}/UD_Latin-${treebank}/MM-la_${treebank,,}-ud-test.conllu \
+    --gold ${golddir}/UD_Latin-${treebank}/${MM}la_${treebank,,}-ud-test.conllu \
     --output $wdir/${treebank}.hypergold.conllu
 done
+
+# UD_Latin-CIRCSE/la_circse-ud-test.conllu
